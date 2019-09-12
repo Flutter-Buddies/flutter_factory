8223b05bbeacd68014aa65e19142b692867af109
lukaknezic <luka.knezic@gmail.com>
2019-09-12 12:35:10 +0200

    Add desktop support

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_factory/ui/screens/main_page.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  TargetPlatform targetPlatform;
+  if (Platform.isMacOS) {
+    targetPlatform = TargetPlatform.iOS;
+  } else if (Platform.isLinux || Platform.isWindows) {
+    targetPlatform = TargetPlatform.android;
+  }
+  if (targetPlatform != null) {
+    debugDefaultTargetPlatformOverride = targetPlatform;
+  }
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
    @override
